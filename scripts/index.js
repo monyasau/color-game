f0e2b0a46f10aaf49e1eaa4db907b2660eae9f6c
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2025-02-03 14:53:19 +0100

    create loop utility to avoid redundant repetition,

--- scripts/index.js
+++ scripts/index.js
@@ -9,9 +9,15 @@ let computedArray=[]
 const targetColorElement = document.querySelector('div[data-testid="colorBox"]');
 targetColorElement.style.backgroundColor = colorArray[Math.floor(Math.random()*colorArray.length)];
 
-for (let index = 0; index < colorArray.length; index++) {
+
+const Loop=(array=[],callback=()=>{})=>{
+    for (let index=0; index < array.length; index++) {
+    callback()
+}}
+
+Loop(colorArray, ()=>{
     const color = colorArray[Math.floor(Math.random()*colorArray.length)];
     if(computedArray.length<=30&&!computedArray.includes(color)){
         computedArray.push(color)
     }
-}
+})
